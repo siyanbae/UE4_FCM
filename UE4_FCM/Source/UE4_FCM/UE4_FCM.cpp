7ac8c601ef9ca99d247d6100f97415b50f67a03b
siyanbae <f1f514@gmail.com>
2021-04-20 09:40:50 +0900

    프로젝트 추가

---
+++ UE4_FCM/Source/UE4_FCM/UE4_FCM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_FCM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_FCM, "UE4_FCM" );
---
+++ UE4_FCM/Source/UE4_FCM/UE4_FCMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_FCMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_FCM_API AUE4_FCMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
